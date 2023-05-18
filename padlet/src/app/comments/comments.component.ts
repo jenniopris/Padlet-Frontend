10db4d0e5e3dc4b3c9b0ea9f7495761c53e2f29f
jenniopris <s2010456021@students.fh-hagenberg.at>
2023-05-18 16:11:54 +0200

    added comments

---
+++ padlet/src/app/comments/comments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pd-comments',
+  templateUrl: './comments.component.html',
+  styles: [
+  ]
+})
+export class CommentsComponent {
+
+}
