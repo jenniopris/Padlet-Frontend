304a9cc0a89c62da329e2ac7681fd078316132be
jenniopris <s2010456021@students.fh-hagenberg.at>
2023-05-19 14:20:13 +0200

    deleting entry in detail view works

--- padlet/src/app/entry-details/entry-details.component.ts
+++ padlet/src/app/entry-details/entry-details.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Entry} from "../shared/entry";
 import {EntryFactory} from "../shared/entry-factory";
 import {PadletApiService} from "../shared/padlet-api.service";
@@ -18,6 +18,7 @@ export class EntryDetailsComponent implements OnInit {
   constructor(
     private ps: PadletApiService,
     private route: ActivatedRoute,
+    private router: Router,
   ) {
   }
 
@@ -25,5 +26,14 @@ export class EntryDetailsComponent implements OnInit {
     const params = this.route.snapshot.params;
     this.buttonRouterLink = '/padlets/' + params['padletId'];
     this.ps.getEntryById(params['entryId']).subscribe(res => this.entry = res);
+  }
+
+  deleteEntry() {
+    console.log("delete entry worked");
+    if (confirm('Are you sure you want to delete this entry (' + this.entry.name + ')?')) {
+      this.ps.deleteEntry(this.entry.id).subscribe(res => {
+        this.router.navigate(['../'], {relativeTo: this.route});
+      });
+    }
   }
 }
