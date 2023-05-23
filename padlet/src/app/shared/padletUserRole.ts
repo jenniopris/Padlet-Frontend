55d714fbcd1c6c09a58abc00430c843ed41f1c38
jenniopris <s2010456021@students.fh-hagenberg.at>
2023-05-23 18:39:10 +0200

    small bugfixes + added invited as and invited by to the invites

--- padlet/src/app/shared/padletUserRole.ts
+++ padlet/src/app/shared/padletUserRole.ts
@@ -1,4 +1,4 @@
-import {Padlet} from "./padlet";
+import {Padlet, User} from "./padlet";
 
 export class PadletUserRole {
   constructor(
@@ -6,7 +6,8 @@ export class PadletUserRole {
     public padlet_id: number,
     public user_id: number,
     public role: string,
-    public padlet?: Padlet
+    public padlet?: Padlet,
+    public invited_by?: string,
   ) {
   }
 }
